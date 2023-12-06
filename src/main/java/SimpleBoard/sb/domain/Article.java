e06247eb50fbf81f119874261980b9ea09773efc
Jung ho Jeon <wjswjdgh03@gmail.com>
2023-12-06 22:03:03 +0900

    Entity, Repository 생성

---
+++ src/main/java/SimpleBoard/sb/domain/Article.java
@@ -0,0 +1,25 @@
+package SimpleBoard.sb.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+    @Column(name = "title", nullable = false)
+    private String title;
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Builder
+    public Article(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
---
+++ src/main/java/SimpleBoard/sb/repository/BlogRepository.java
@@ -0,0 +1,7 @@
+package SimpleBoard.sb.repository;
+
+import SimpleBoard.sb.domain.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BlogRepository extends JpaRepository<Article,Long> {
+}
