5f7488924f2689aa0f2035178b137c8bc26a7a30
Jung ho Jeon <wjswjdgh03@gmail.com>
2024-01-04 00:28:00 +0900

    수정 등록 추가

--- src/main/resources/static/js/article.js
+++ src/main/resources/static/js/article.js
@@ -1,4 +1,6 @@
 const deleteButton = document.getElementById('delete-btn');
+const modifyButton = document.getElementById('modify-btn');
+const modifyButton = document.getElementById('create-btn');
 
 if (deleteButton) {
     deleteButton.addEventListener('click', event => {
@@ -11,4 +13,43 @@ if (deleteButton) {
                 location.replace('/articles');
             });
     });
+}
+if (modifyButton) {
+    modifyButton.addEventListener('click', event => {
+        let params = new URLSearchParams(location.search);
+        let id = params.get('id');
+
+        fetch(`/api/articles/${id}`, {
+            method: 'PUT',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                title: document.getElementById('title').value,
+                content: document.getElementById('content').value
+            })
+        })
+            .then(() => {
+                alert('수정이 완료되었습니다.');
+                location.replace(`/articles/${id}`);
+            });
+    });
+
+    if (createButton) {
+        createButton.addEventListener('click', event => {
+            fetch(`/api/articles`, {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    title: document.getElementById('title').value,
+                    content: document.getElementById('content').value
+                })
+            })
+                .then(() => {
+                    alert('등록 완료되었습니다.');
+                    location.replace(`/articles`);
+                });
+        });
 }
