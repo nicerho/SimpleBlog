184ca41143099c65e627eb0160047cbd3dadd00c
Jung ho Jeon <wjswjdgh03@gmail.com>
2024-01-04 13:25:08 +0900

    필터체인 설정

---
+++ src/main/java/SimpleBoard/sb/dto/AddUserRequest.java
@@ -0,0 +1,11 @@
+package SimpleBoard.sb.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AddUserRequest {
+    private String email;
+    private String password;
+}
---
+++ src/main/java/SimpleBoard/sb/service/UserService.java
@@ -0,0 +1,22 @@
+package SimpleBoard.sb.service;
+
+import SimpleBoard.sb.domain.User;
+import SimpleBoard.sb.dto.AddUserRequest;
+import SimpleBoard.sb.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Long save(AddUserRequest dto) {
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+    }
+}
